01050b98d1dda3fc17ca05a8f4fb33d6e6bd37fc
Satish Rathod <139036744+satish-rathod@users.noreply.github.com>
2023-09-25 23:43:46 +0530

    Update traping_water.java

--- Course/T-1/ICP/09 22/traping_water.java
+++ Course/T-1/ICP/09 22/traping_water.java
@@ -11,7 +11,7 @@ public class traping_water {
         for (int i = 1; i < n; i++) {
             left[i] = Math.max(left[i-1], arr[i]);
         }
-        System.out.println(Arrays.toString(left));
+       // System.out.println(Arrays.toString(left));
         right[n-1] = arr[n-1];
         for (int i = n-2; i >= 0; i--) {
             right[i] = Math.max(right[i+1], arr[i]);
